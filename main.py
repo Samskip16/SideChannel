d72b06e7e344ca9a0e5404ff1d973410c41f03a0
sam <samjansen16@gmail.com>
2019-03-30 12:47:53 +0100

    Working ex1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.io as sio
-import matplotlib.pyplot as plt
+from bokeh.plotting import figure
+from bokeh.io import show
 
 
 def xor(val1, val2):
@@ -30,49 +31,94 @@ def s_box(val):
     return switch.get(val, 0)
 
 
-def hw(x):
-    return bin(x).count("1")
+def hw(val):
+    return bin(val).count("1")
 
 
-def attack(nr):
-    inn = sio.loadmat('in.mat')['in']
-
-    val_pred = np.empty((nr, 16), dtype=int)
-    val_power = np.empty((nr, 16), dtype=int)
-    corr_arr = np.empty((6990, 16))
-    totals = dict()
-
-    for i in range(0, nr):
-        for j in range(0, 16):
-            inn_val = inn[i][0]
-            val_pred[i, j] = s_box(xor(inn_val, j))
-
-    for i in range(0, nr):
-        for j in range(0, 16):
-            val = val_pred[i][j]
-            val_power[i, j] = hw(val)
-
-    traces = sio.loadmat('traces.mat')['traces'][:nr, :]
-    print(traces.shape)
+def process_row_correlation(corr_mtrx):
+    d = dict()
 
     for i in range(0, 16):
-        total = 0
-        for j in range(0, 6990):
-            cor = abs(np.multiarray.correlate(traces[:, j], val_power[:, i]))
-            total += cor
+        row_corr_data = corr_mtrx[i, :]
+        correlation = np.max(np.abs(row_corr_data))
+        d[correlation] = i
 
-            corr_arr[j][i] = cor
+    row_correlation = np.arange(16)
+    for i, (key, value) in enumerate(sorted(d.items(), reverse=True)):
+        row_correlation[i] = value
 
-        totals[str(i)] = total
-
-    sorted_t = sorted(totals.items(), key=lambda kv: kv[1])
-    best_k = int(sorted_t[15][0])
-
-    print(best_k)
-
-    lines = plt.plot(corr_arr, 'b')
-    plt.setp(lines[best_k], color='r')
-    plt.show()
+    return row_correlation
 
 
-attack(2)
+def plot_row_correlation(row_correlation, max_row, nr_of_traces):
+    colors = []
+    for i in row_correlation:
+        colors.append('red' if i == max_row else 'blue')
+
+    inc_one = lambda x: x + 1
+    legend = 'Row correlation per candidate ' + str(nr_of_traces) + ' power traces attack'
+
+    plot = figure(plot_width=900, plot_height=600)
+    plot.vbar(x=np.arange(1, 17), top=inc_one(row_correlation), width=0.9, color=colors, legend=legend)
+    plot.xaxis.axis_label = 'Candidate ranking'
+    plot.yaxis.axis_label = 'Candidate nr.'
+    show(plot)
+
+
+def plot_correlation(corr_mtrx, max_row):
+    plot = figure(plot_width=1200, plot_height=800)
+    x_values = np.arange(6990)
+
+    for i in range(0, 16):
+        y_values = corr_mtrx[i, :]
+        color = 'red' if i == max_row else 'blue'
+
+        plot.line(x_values, y_values, color=color, legend='Absolute correlation')
+
+    show(plot)
+
+
+def attack(nr_of_traces):
+    inn = sio.loadmat('in.mat')['in']
+    traces = sio.loadmat('traces.mat')['traces'][:nr_of_traces, :]
+
+    power_mtrx = np.empty((nr_of_traces, 16), dtype=int)
+
+    for i in range(0, nr_of_traces):
+        for j in range(0, 16):
+            inn_val = inn[i][0]
+            power_mtrx[i, j] = hw(s_box(xor(inn_val, j)))
+
+    corr_mtrx = np.corrcoef(traces, power_mtrx, rowvar=False)
+    corr_mtrx = corr_mtrx[: len(power_mtrx[0]), len(power_mtrx[0]):]
+
+    return corr_mtrx
+
+
+def attack_total():
+    corr_mtrx = attack(14900)
+
+    row_correlation = process_row_correlation(corr_mtrx)
+    max_row = row_correlation[0]
+
+    plot_correlation(corr_mtrx, max_row)
+
+    return max_row
+
+
+def attack_nr(nr_of_traces, max_row):
+    corr_mtrx = attack(nr_of_traces)
+
+    row_correlation = process_row_correlation(corr_mtrx)
+    plot_row_correlation(row_correlation, max_row, nr_of_traces)
+
+
+max_row = attack_total()
+
+attack_nr(500, max_row)
+attack_nr(1000, max_row)
+attack_nr(2000, max_row)
+attack_nr(4000, max_row)
+attack_nr(8000, max_row)
+attack_nr(12000, max_row)
+attack_nr(14900, max_row)
